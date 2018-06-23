ee05fd395c2f8159b1c922017947927c8179e21a
Grinish <Grinish@gmail.com>
2018-06-23 08:23:53 -0700

    added Demo project

---
+++ demo/src/main/java/com/demo/test/MyTesting.java
@@ -0,0 +1,9 @@
+package com.demo.test;
+
+public class MyTesting {
+
+    public static void main(String args[]) {
+
+
+    }
+}
---
+++ demo/src/main/java/com/demo/test/MyThreadSleep.java
@@ -0,0 +1,22 @@
+package com.demo.test;
+
+public class MyThreadSleep extends Thread{
+
+    public void run(){
+        for(int i=1;i<5;i++){
+            try{
+                Thread.sleep(500);
+            }catch(InterruptedException e){
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
+    }
+    public static void main(String args[]){
+        MyThreadSleep t1=new MyThreadSleep();
+        MyThreadSleep t2=new MyThreadSleep();
+
+        t1.start();
+        t2.start();
+    }
+}
